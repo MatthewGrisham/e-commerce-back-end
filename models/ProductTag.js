19dad6776aedba0d3f2b4811ad9b8670241dc860
Matt Grisham <magrishams@gmail.com>
2022-08-10 16:21:00 -0500

    updates to productTag.js and several type fixes.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,7 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
